e107fb6b64608b4d9a2c16a6175a571ad151778c
Catherine114 <shunaev80@mail.ru>
2022-03-26 15:24:54 +0300

    'project2'

--- nuxt-blog-master/components/HelloComponent.vue
+++ nuxt-blog-master/components/HelloComponent.vue
@@ -1,12 +1,5 @@
 <template>
   <div>
-    <p>
-      <span id="one">Б</span>
-      <span id="two">Л</span>
-      <span id="three">О</span>
-      <span id="four">Г</span>
-  
-    </p>
     <h1>ЭТО СУПЕР БЛОГ</h1>
   </div>
 </template>
@@ -26,49 +19,12 @@ div {
   flex-direction: column;
 }
 h1 {
-  text-align: center;
-}
-p {
   color: #0017ff;
   text-align: center;
   font-family: Oswald, Arial;
-  font-size: 24vmin;
+  font-size: 10vmin;
   letter-spacing: 0vmin;
-  text-shadow: 1vmin 2vmin black;
-}
-#one {
-  position: relative;
-}
-#two {
-  position: relative;
-  color: #ff6b42;
-
-  animation-delay: 1.2s;
-}
-#three {
-  position: relative;
-  color: #ffbc36;
-
-  animation-delay: 1.2s;
-}
-#four {
-  position: relative;
-
-  animation-delay: 1.3s;
+  text-shadow: 1vmin 1vmin black;
 }
 
-@keyframes bounce {
-  0% {
-    top: -5vmin;
-  }
-  25% {
-    top: -15vmin;
-  }
-  50% {
-    top: -10vmin;
-  }
-  100% {
-    top: -5vmin;
-  }
-}
 </style>
